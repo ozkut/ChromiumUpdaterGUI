b33eaae2bfb5498ce477601736dfc221f23cffad
ozkut <alpozbaykut@gmail.com>
2021-11-30 20:20:18 +0200

    Initial Commit

---
+++ Form.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ChromiumUpdaterGUI
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_CheckUpdate = new System.Windows.Forms.Button();
+            this.l_CurrentVersion = new System.Windows.Forms.Label();
+            this.l_NewestVersion = new System.Windows.Forms.Label();
+            this.cb_Startup = new System.Windows.Forms.CheckBox();
+            this.b_Exit = new System.Windows.Forms.Button();
+            this.cb_CheckUpateOnClick = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // b_CheckUpdate
+            // 
+            this.b_CheckUpdate.Location = new System.Drawing.Point(12, 66);
+            this.b_CheckUpdate.Name = "b_CheckUpdate";
+            this.b_CheckUpdate.Size = new System.Drawing.Size(124, 27);
+            this.b_CheckUpdate.TabIndex = 0;
+            this.b_CheckUpdate.Text = "Check for updates";
+            this.b_CheckUpdate.UseVisualStyleBackColor = true;
+            this.b_CheckUpdate.Click += new System.EventHandler(this.b_CheckUpdate_Click);
+            // 
+            // l_CurrentVersion
+            // 
+            this.l_CurrentVersion.AutoSize = true;
+            this.l_CurrentVersion.Location = new System.Drawing.Point(12, 9);
+            this.l_CurrentVersion.Name = "l_CurrentVersion";
+            this.l_CurrentVersion.Size = new System.Drawing.Size(105, 17);
+            this.l_CurrentVersion.TabIndex = 1;
+            this.l_CurrentVersion.Text = "Current Version: ";
+            // 
+            // l_NewestVersion
+            // 
+            this.l_NewestVersion.AutoSize = true;
+            this.l_NewestVersion.Location = new System.Drawing.Point(12, 36);
+            this.l_NewestVersion.Name = "l_NewestVersion";
+            this.l_NewestVersion.Size = new System.Drawing.Size(105, 17);
+            this.l_NewestVersion.TabIndex = 2;
+            this.l_NewestVersion.Text = "Newest Version: ";
+            // 
+            // cb_Startup
+            // 
+            this.cb_Startup.AutoSize = true;
+            this.cb_Startup.Location = new System.Drawing.Point(220, 39);
+            this.cb_Startup.Name = "cb_Startup";
+            this.cb_Startup.Size = new System.Drawing.Size(105, 21);
+            this.cb_Startup.TabIndex = 4;
+            this.cb_Startup.Text = "Start on boot";
+            this.cb_Startup.UseVisualStyleBackColor = true;
+            this.cb_Startup.CheckedChanged += new System.EventHandler(this.cb_Startup_CheckedChanged);
+            // 
+            // b_Exit
+            // 
+            this.b_Exit.Location = new System.Drawing.Point(355, 70);
+            this.b_Exit.Name = "b_Exit";
+            this.b_Exit.Size = new System.Drawing.Size(75, 23);
+            this.b_Exit.TabIndex = 5;
+            this.b_Exit.Text = "Exit";
+            this.b_Exit.UseVisualStyleBackColor = true;
+            this.b_Exit.Click += new System.EventHandler(this.b_Exit_Click);
+            // 
+            // cb_CheckUpateOnClick
+            // 
+            this.cb_CheckUpateOnClick.AutoSize = true;
+            this.cb_CheckUpateOnClick.Checked = true;
+            this.cb_CheckUpateOnClick.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_CheckUpateOnClick.Location = new System.Drawing.Point(220, 12);
+            this.cb_CheckUpateOnClick.Name = "cb_CheckUpateOnClick";
+            this.cb_CheckUpateOnClick.Size = new System.Drawing.Size(210, 21);
+            this.cb_CheckUpateOnClick.TabIndex = 6;
+            this.cb_CheckUpateOnClick.Text = "Check for updates on maximise";
+            this.cb_CheckUpateOnClick.UseVisualStyleBackColor = true;
+            this.cb_CheckUpateOnClick.CheckedChanged += new System.EventHandler(this.cb_CheckUpateOnClick_CheckedChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 101);
+            this.Controls.Add(this.cb_CheckUpateOnClick);
+            this.Controls.Add(this.b_Exit);
+            this.Controls.Add(this.cb_Startup);
+            this.Controls.Add(this.l_NewestVersion);
+            this.Controls.Add(this.l_CurrentVersion);
+            this.Controls.Add(this.b_CheckUpdate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chromium Updater";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_CheckUpdate;
+        private System.Windows.Forms.Label l_CurrentVersion;
+        private System.Windows.Forms.Label l_NewestVersion;
+        private System.Windows.Forms.Label l_ProgramStatus;
+        private System.Windows.Forms.CheckBox cb_Startup;
+        private System.Windows.Forms.Button b_Exit;
+        private System.Windows.Forms.CheckBox cb_CheckUpateOnClick;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+//Chromium Updater made by Ozkut
+//This is the GUI version of Chromium Updater
+
+namespace ChromiumUpdaterGUI
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        //initilize variables
+        static NotifyIcon notifyIcon = new() { Visible = true, Icon = System.Drawing.SystemIcons.Application };
+        HttpClient client = new();
+
+        //initilize constants
+        const string appTitle = "Chromium Updater";
+        const string storedVariables = "ChromiumUpdater_StoredVariables.txt";
+
+        const int notificationTimeout = 3000;
+
+        const string StartOnBoot = "StartOnBoot=";
+        const string CheckUpdateOnClick = "CheckUpdateOnClick=";
+
+        public Form() => InitializeComponent();
+
+        private async void Form1_Load(object sender, EventArgs e) 
+        {
+            Hide();
+            await CheckForUpdate();
+
+            if (File.Exists(storedVariables))
+            {
+                UpdateCheckboxesFromFile();
+                if (cb_Startup.Checked)
+                    CheckStartupStatus(cb_Startup.Checked);
+            }
+                
+            else
+                CreateStoredVariables();
+
+            notifyIcon.BalloonTipClicked += NotifyIconClicked;
+            notifyIcon.Click += NotifyIconClicked;
+        }
+
+        private async void NotifyIconClicked(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+
+            if (cb_CheckUpateOnClick.Checked)
+                await CheckForUpdate();
+        }
+
+        private void UpdateCheckboxesFromFile()
+        {
+            UpdateFileAttributes(false);
+
+            const string True = "True";
+            const string False = "False";
+
+            foreach (string line in File.ReadAllLines(storedVariables))
+            {
+                switch (line)
+                {
+                    case StartOnBoot + True:
+                        cb_Startup.Checked = true;
+                        break;
+
+                    case StartOnBoot + False:
+                        cb_Startup.Checked = false;
+                        break;
+
+                    case CheckUpdateOnClick + True:
+                        cb_CheckUpateOnClick.Checked = true;
+                        break;
+
+                    case CheckUpdateOnClick + False:
+                        cb_CheckUpateOnClick.Checked = false;
+                        break;
+                }
+            }
+
+            UpdateFileAttributes(true);
+        }
+
+        //storedVaribles stuff
+        private void CreateStoredVariables()
+        {
+            File.WriteAllText(storedVariables,
+                              StartOnBoot + cb_Startup.Checked.ToString() + "\n" +
+                              CheckUpdateOnClick + cb_CheckUpateOnClick.Checked.ToString() + "\n");
+
+            UpdateFileAttributes(true);
+        }
+
+        private async void UpdateStoredVariables()
+        {
+            if (File.Exists(storedVariables))
+            {
+                UpdateFileAttributes(false);
+
+                string[] temp_StoredVariables = File.ReadAllLines(storedVariables);
+                string[] final_StoredVariables = new string[temp_StoredVariables.Length];
+
+                for (int i = 0; i < temp_StoredVariables.Length; i++)
+                {
+                    if (temp_StoredVariables[i] == $"{StartOnBoot}True" || temp_StoredVariables[i] == $"{StartOnBoot}False")
+                        final_StoredVariables[i] = StartOnBoot + cb_Startup.Checked;
+
+                    else if (temp_StoredVariables[i] == $"{CheckUpdateOnClick}True" || temp_StoredVariables[i] == $"{CheckUpdateOnClick}False")
+                        final_StoredVariables[i] = CheckUpdateOnClick + cb_CheckUpateOnClick.Checked;
+
+                    else
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                final_StoredVariables[i] = StartOnBoot + cb_Startup.Checked;
+                                break;
+
+                            case 1:
+                                final_StoredVariables[i] = CheckUpdateOnClick + cb_CheckUpateOnClick.Checked;
+                                break;
+                        }
+                    }
+
+                    for (int j = 0; j < final_StoredVariables.Length; j++)
+                    {
+                        if (j == 0)
+                            await File.WriteAllTextAsync(storedVariables, final_StoredVariables[j] + "\n");//throws access denied exception
+                        else
+                            await File.AppendAllTextAsync(storedVariables, final_StoredVariables[j] + "\n");
+                    }
+                }
+                UpdateFileAttributes(true);
+            }
+
+            else
+                CreateStoredVariables();
+        }
+
+        private static void UpdateFileAttributes(bool hideFile)
+        {
+            FileAttributes attributes = File.GetAttributes(storedVariables);
+
+            if (hideFile)
+                File.SetAttributes(storedVariables, attributes | FileAttributes.Hidden);
+            else
+            {
+                attributes &= ~FileAttributes.Hidden;
+                File.SetAttributes(storedVariables, attributes);
+            }
+        }
+
+        //the original methods
+        private static void CheckStartupStatus(bool startOnBoot)
+        {
+            string programPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), appTitle.Replace(' ', '.') + ".exe");
+            string currentProgramPath = Process.GetCurrentProcess().MainModule.FileName;
+
+            switch (startOnBoot)
+            {
+                case true:
+                    if (File.Exists(programPath))
+                        File.Replace(currentProgramPath, programPath, null);
+                    else
+                        File.Copy(currentProgramPath, programPath);
+                    break;
+
+                case false:
+                    File.Delete(programPath);
+                    break;
+            }
+        }
+
+        private async Task CheckForUpdate()
+        {
+            string newestVersion = string.Empty;
+            string currentVersion = string.Empty;
+            bool hasInternet = false;
+
+            try
+            {
+                newestVersion = await client.GetStringAsync("https://omahaproxy.appspot.com/win");
+                currentVersion = FileVersionInfo.GetVersionInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Chromium\Application\chrome.exe")).ProductVersion;
+                hasInternet = true;
+            }
+            catch (HttpRequestException e)
+            {
+                DisplayErrorMessage("Can't get the latest version number of Chromium.\n" +
+                                    "Please check your internet connection.", e);
+                hasInternet = false;
+
+            }
+            catch (FileNotFoundException e) 
+            { 
+                DisplayErrorMessage($"A valid install of Chromium could not be found at {Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Chromium\Application\chrome.exe")}\n" +
+                                    "Please make sure that Chromium is installed at thet locatiom.", e); 
+            }
+            catch (Exception e) 
+            { 
+                DisplayErrorMessage("An unknown error has occured.\n" + 
+                                    e.Message.ToString(), e); 
+            }
+
+            //don't know if this is necessary or if i just just hard-code the "newest version:" and "current version:"
+            //into the strings rather than adding the version numbers on top and removing the extras every time
+            if (l_CurrentVersion.Text.Length > currentVersion.Length || l_NewestVersion.Text.Length > newestVersion.Length)
+            {
+                l_CurrentVersion.Text = l_CurrentVersion.Text.Trim(currentVersion.ToCharArray());
+                l_NewestVersion.Text = l_NewestVersion.Text.Trim(newestVersion.ToCharArray());
+            }
+
+            l_CurrentVersion.Text += currentVersion;
+            l_NewestVersion.Text += newestVersion;
+
+            if (hasInternet)
+            {
+                if (currentVersion == newestVersion)
+                    IsUpToDate(Visible);
+
+                else
+                {
+                    if (!Visible)
+                        notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, "An update is available!", ToolTipIcon.None);
+
+                    else
+                    {
+                        DialogResult updateAvailableDialog =
+                        MessageBox.Show($"Current Version: {currentVersion}\n" +
+                                        $"Newest version available: {newestVersion}\n" +
+                                        "Would you like to update?",
+                                        appTitle,
+                                        MessageBoxButtons.YesNo,
+                                        MessageBoxIcon.Question);
+
+                        if (updateAvailableDialog == DialogResult.Yes)
+                            await DownloadNewestVersion();
+                    }
+                }
+            }
+        }
+
+        private async Task DownloadNewestVersion()
+        {
+            notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, "Downloading update file", ToolTipIcon.Info);
+
+            byte[] file;
+            string fileLocation = string.Empty;
+            const string fileName = "mini_installer.sync.exe";
+
+            try
+            {
+                file = await client.GetByteArrayAsync("https://github.com/Hibbiki/chromium-win64/releases/latest/download/mini_installer.sync.exe");
+                fileLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+                File.WriteAllBytes(fileLocation, file);
+            }
+            catch (HttpRequestException e)
+            {
+                DisplayErrorMessage("An HTTP error has occured when trying to download the latest version of Chromium.\n" +
+                                    "Please check your internet connection.", new Exception(e.Message));
+            }
+            catch (PathTooLongException e) 
+            { 
+                DisplayErrorMessage($"The path '{fileLocation}' is too long.\n"/* +
+                                    "Please enter a shorter file path."*/, e);
+                //second part will be used when the option to sellect a custom location is added
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                DisplayErrorMessage("The program does not have the proper permissions to write to " +
+                                    $"'{fileLocation.Trim(fileName.ToCharArray())}'\n" +
+                                    "Please check if the program has the correct permissions to write to the given directory.", e);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                DisplayErrorMessage($"The directory '{fileLocation.Trim(fileName.ToCharArray())}' could not be found.\n" +
+                                    "Please make sure that the directory exists.", e);
+            }
+            catch (IOException e) 
+            {
+                DisplayErrorMessage("An I/O error occured when trying to write the upadte file to " +
+                                    $"'{fileLocation.Trim(fileName.ToCharArray())}'\n" +
+                                    "Please make sure that the directory isn't read-only.", e);
+            }
+            catch (Exception e) 
+            {
+                DisplayErrorMessage("An unknown error has occured.\n" + 
+                                    e.Message.ToString(), e);
+            }
+
+            notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, "Update file downloaded succesfully", ToolTipIcon.Info);
+
+            Process[] processes = Process.GetProcessesByName("chrome");
+
+            if (processes.Length > 0)
+            {
+                DialogResult browserOpenDialog =
+                MessageBox.Show("An instance of Chromium is already running!\n" +
+                                "Would you like to close it?",
+                                appTitle,
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Exclamation);
+
+                if (browserOpenDialog == DialogResult.Yes)
+                {
+                    foreach (Process process in processes)
+                        process.Kill();
+                }
+
+                DoUpdate(fileLocation, Visible);
+            }
+
+            else
+                DoUpdate(fileLocation, Visible);
+
+            DialogResult deleteDialog =
+            MessageBox.Show($"Would you like to delete the update file from {fileLocation}?",
+                            appTitle,
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question);
+
+            if (deleteDialog == DialogResult.Yes)
+                File.Delete(fileLocation);
+        }
+
+        private static void IsUpToDate(bool isVisible)
+        {
+            const string upToDateText = "Chromium is up-to-date!";
+
+            if (!isVisible)
+                notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, upToDateText, ToolTipIcon.None);
+
+            else
+                MessageBox.Show(upToDateText, appTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private static void DoUpdate(string fileLocation, bool isVisible)
+        {
+            notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, "Installing Update", ToolTipIcon.Info);
+
+            Process updater = Process.Start(fileLocation);
+            updater.WaitForExit();
+
+            notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, "Update Complete!", ToolTipIcon.Info);
+            
+            if (isVisible)
+            {
+                System.Threading.Thread.Sleep(750);
+                MessageBox.Show("Update Complete!", appTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //checkboxes
+        private void cb_Startup_CheckedChanged(object sender, EventArgs e) 
+        { 
+            UpdateStoredVariables();
+            CheckStartupStatus(cb_Startup.Checked);
+        }
+
+        private void cb_CheckUpateOnClick_CheckedChanged(object sender, EventArgs e) => UpdateStoredVariables();
+
+        //buttons
+        private async void b_CheckUpdate_Click(object sender, EventArgs e) => await CheckForUpdate();
+
+        private void b_Exit_Click(object sender, EventArgs e) => ExitProgram();
+
+        //program end stuff
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing || e.CloseReason == CloseReason.None)
+            {
+                Hide();
+                e.Cancel = true;
+            }
+        }
+
+        private void DisplayErrorMessage(string errorMessage, Exception e)
+        {
+            if (e is HttpRequestException)
+            {
+                if (!Visible)
+                    notifyIcon.ShowBalloonTip(notificationTimeout, appTitle, errorMessage, ToolTipIcon.Warning);
+                else
+                    MessageBox.Show(errorMessage, appTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return;
+            }  
+
+            string errorLogLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ChromiumUpdater_Error.Log");
+            MessageBox.Show("The program has crashed with the following exception:\n" + errorMessage + 
+                            $"\nAn error log has been created at: '{errorLogLocation}'", 
+                            appTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            File.WriteAllText(errorLogLocation, e.ToString());
+            Environment.Exit(1);
+        }
+
+        private void ExitProgram()
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            client.Dispose();
+            Environment.Exit(0);
+        }
+    }
+}
